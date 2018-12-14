c16a1fc054263a3ef24199a757d03a4c177c80a5
Sam Montelongo <smontelongo@insite.net>
2018-12-14 08:55:45 -0600

    Testing changes in urls.py

--- wagtailenforcer/urls.py
+++ wagtailenforcer/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib.auth import views as django_auth_views
 
 from wagtail.admin.views import account as wagtail_account_views
+from wagtail.admin.views.account import LoginView
 
 from axes.decorators import watch_login
 
@@ -17,7 +18,9 @@ urlpatterns = [
     url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', account_views.password_reset_confirm, name='wagtailadmin_password_reset_confirm'),
     url(r'^password_reset/complete/$', account_views.password_reset_complete, name='wagtailadmin_password_reset_complete'),
 
-    # url(r'^login/$', watch_login(wagtail_account_views.login), name='wagtailadmin_login'),
+    # TODO Review - changed login/ to admin/
+    url(r'^login/$', watch_login(LoginView), name='wagtailadmin_login'),
+
     url(r"^account/change_password/", change_password, name="password_change"),
     url(r'^users/(\d+)/$', users.edit, name='wagtailusers_users_edit'),
     url(r'^users/new/$', users.create, name='wagtailusers_users_create'),
